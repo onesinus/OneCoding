3340367f1da7efb8b3a53d93b7fac855412d51c3
Onesinus SPT <onesinus.parulian@bidbox.id>
2021-02-11 08:01:24 +0700

    Create simple list embeded youtube video

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
+
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { ListItem, ThemeProvider } from 'react-native-elements';
+import { useColorScheme } from 'react-native-appearance';
+
+
+import YoutubeView from './components/YoutubeView'
 
 export default function App() {
+  let colorScheme = useColorScheme();
+
+  const videos = [
+    {key: 1, videoId: "BpiRMVV_WhE"},
+    {key: 2, videoId: "22FfQHW_YFE"},
+    {key: 3, videoId: "LgeQR3sFiRI"},
+    {key: 4, videoId: "h5DK0JzICdM"},
+    {key: 5, videoId: "rAwIdpQEsCU"}, 
+  ]
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <SafeAreaProvider>
+        <ThemeProvider useDark={colorScheme === 'dark'}>
+          <ScrollView>
+            { videos.map((video) => (
+              <ListItem style={{ marginTop: 30 }} key={video.key} bottomDivider>
+                  <YoutubeView
+                    key={video.key}
+                    videoId={video.videoId}
+                  />
+              </ListItem>
+            ))}
+          </ScrollView>
+        </ThemeProvider>        
+      </SafeAreaProvider>
     </View>
   );
 }
---
+++ components/YoutubeView.js
@@ -0,0 +1,31 @@
+import React, { useState, useCallback, useRef } from "react";
+import { Button, View, Alert } from "react-native";
+import YoutubePlayer from "react-native-youtube-iframe";
+
+export default function App({videoId}) {
+  const [playing, setPlaying] = useState(false);
+
+  const onStateChange = useCallback((state) => {
+    if (state === "ended") {
+      setPlaying(false);
+    //   Alert.alert("video has finished playing!");
+    }
+  }, []);
+
+  const togglePlaying = useCallback(() => {
+    setPlaying((prev) => !prev);
+  }, []);
+
+  return (
+    <View>
+      <YoutubePlayer
+        height={195}
+        width={330}
+        play={playing}
+        videoId={videoId}
+        onChangeState={onStateChange}
+      />
+      {/* <Button title={playing ? "pause" : "play"} onPress={togglePlaying} /> */}
+    </View>
+  );
+}
